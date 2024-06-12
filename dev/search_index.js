59130c21f6ea25b5176d2ebf517bc00613c1ffe7
Documenter.jl <documenter@juliadocs.github.io>
2024-06-12 15:01:00 +0000

    build based on 055097d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ArcParser","category":"page"},{"location":"#ArcParser","page":"Home","title":"ArcParser","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ArcParser.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ArcParser]","category":"page"}]
+}
