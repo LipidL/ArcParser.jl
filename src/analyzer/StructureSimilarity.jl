d940736b7b477a2ee69622c056a80a0e49e01b58
lipid <lipid.l@qq.com>
2024-06-13 18:19:06 +0800

    implemented kabsch method for similarity calculation

--- src/analyzer/StructureSimilarity.jl
+++ src/analyzer/StructureSimilarity.jl
@@ -1,6 +1,9 @@
 using ArcParser
 using LinearAlgebra
-function calculate_similarity(structure1::Vector{Atom{T}}, structure2::Vector{Atom{T}}, θ::T) where T
+abstract type AbstractSimilarityMeasurement end
+struct SimplifiedSOAP <: AbstractSimilarityMeasurement end
+struct Kabsch <: AbstractSimilarityMeasurement end
+function calculate_similarity(structure1::Vector{Atom{T}}, structure2::Vector{Atom{T}}, ::SimplifiedSOAP, θ::T) where T
     n_atoms = length(structure1)
     if n_atoms != length(structure2)
         return -Inf64
@@ -16,4 +19,25 @@ function calculate_similarity(structure1::Vector{Atom{T}}, structure2::Vector{At
     end
     similarity /= n_atoms
     return similarity
+end
+
+function calculate_similarity(struct1::Matrix{T}, struct2::Matrix{T}, ::Kabsch ) where T
+    if size(struct1, 2) != 3 || size(struct2, 2) != 3
+        error("Input matrix should be a n*3 matrix")
+    end
+    if size(struct1, 1) != size(struct2, 1)
+        error("Two structures should have the same number of atoms")
+    end
+    num_atoms = size(struct1, 1)
+    center1 = sum(struct1, dims=2) / size(struct1, 1)
+    center2 = sum(struct2, dims=2) / size(struct2, 1)
+    moved1 = struct1 .- center1
+    moved2 = struct2 .- center2
+    H = moved1' * moved2
+    U, S, V = svd(H)
+    d = sign(det(U) * det(V))
+    R = U * Diagonal([1, 1, d]) * V'
+    moved1 = moved1 * R
+    rmsd = LinearAlgebra.norm(moved1 - moved2) / sqrt(num_atoms)
+    return rmsd
 end
--- test/analyzer.jl
+++ test/analyzer.jl
@@ -3,17 +3,31 @@ using Test
 using Random
 
 @testset "analyzer.jl" begin
-    # Test AtomDataParser
+    # Test similarity
     println(pwd())
     structures = ArcParser.read_arc("test1.arc")
     atoms = structures[1].atoms
-    for i in range(1, 1)
+    for i in range(1, 10)
         # Randomly select 2 groups with 6 elements each, where the element field is "Fe"
         selected_groups = filter(x -> x.element == "Fe", atoms)
         selected_indices = rand(1:length(selected_groups), 12)
         selected_groups = selected_groups[selected_indices]
         group1 = selected_groups[1:6]
         group2 = selected_groups[7:12]
-        @test ArcParser.calculate_similarity(group1, group2, 1e-3) isa AbstractFloat
+        # Test SimplifiedSOAP
+        @test ArcParser.calculate_similarity(group1, group2, ArcParser.SimplifiedSOAP(), 0.1) isa AbstractFloat
+        @test ArcParser.calculate_similarity(group1, group2, ArcParser.SimplifiedSOAP(), 0.1) >= 0.0
+        @test ArcParser.calculate_similarity(group1, group1, ArcParser.SimplifiedSOAP(), 0.1) >= 1.0
+        @test ArcParser.calculate_similarity(group2, group2, ArcParser.SimplifiedSOAP(), 0.1) >= 1.0
+        gp1_matrix = Matrix{Float64}(undef, length(group1),3)
+        gp2_matrix = Matrix{Float64}(undef, length(group2),3)
+        for i in range(1, length(group1))
+            gp1_matrix[i, :] = ArcParser.extract_position(group1[i])
+        end
+        for i in range(1, length(group2))
+            gp2_matrix[i, :] = ArcParser.extract_position(group2[i])
+        end
+        @test ArcParser.calculate_similarity(gp1_matrix, gp2_matrix, ArcParser.Kabsch()) isa AbstractFloat
+        @test ArcParser.calculate_similarity(gp1_matrix, gp2_matrix, ArcParser.Kabsch()) >= 0.0
     end
 end
